ede4ebd7de43339df08fdd105cc30af142d3d560
Andrew Marcinkevičius <andrew.web@ifdattic.com>
2017-10-14 10:43:20 +0200

    Add default Dinosaur entity

---
+++ tutorial/src/AppBundle/Entity/Dinosaur.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="dinosaurs")
+ */
+class Dinosaur
+{
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $length = 0;
+
+    public function getLength(): int
+    {
+        return $this->length;
+    }
+
+    public function setLength(int $length)
+    {
+        $this->length = $length;
+    }
+
+    public function getSpecification(): string
+    {
+        return '';
+    }
+}
