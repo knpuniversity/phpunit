bf21175621a19bbae5b8b82b3a08ad58ec8013d8
Ryan Weaver <ryan@thatsquality.com>
2017-11-01 14:39:38 -0400

    simpler

--- tutorial/src/AppBundle/Entity/Dinosaur.php
+++ tutorial/src/AppBundle/Entity/Dinosaur.php
@@ -15,13 +15,4 @@ class Dinosaur
      */
     private $length = 0;
 
-    public function getLength(): int
-    {
-        return $this->length;
-    }
-
-    public function setLength(int $length)
-    {
-        $this->length = $length;
-    }
 }
